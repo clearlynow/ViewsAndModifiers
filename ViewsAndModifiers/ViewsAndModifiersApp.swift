7d5b4e4d84d574035ce6b07fa8452f037c771665
clearlynow <59779189+clearlynow@users.noreply.github.com>
2020-12-29 11:10:57 -0700

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Alison Gorman on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

90b3d11303df7b81e372f97e043a2a74aa054821
clearlynow <59779189+clearlynow@users.noreply.github.com>
2020-12-29 13:22:41 -0700

    initial commit

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -3,14 +3,45 @@
 //  ViewsAndModifiers
 //
 //  Created by Alison Gorman on 12/29/20.
-//
+//  100 Days of Swift
+//  Day 24 - Project 3 - Challenge
+//  Create a custom ViewModifier (and accompanying View extension) that makes a view have a large, blue font suitable for prominent titles in a view.
 
 import SwiftUI
 
+struct BigBlue: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+            .padding()
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(BigBlue())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading){
+            Text("""
+                    100 Days of SwiftUI
+                    Day 24
+                    Views and Modifiers
+                    """)
+                .titleStyle()
+            Text("Challenge 1. ")
+                .padding()
+            Text("""
+            Create a custom ViewModifier (and accompanying View extension) that makes a view have a large, blue font suitable for prominent titles in a view.
+            """)
+                .font(.body)
+                .padding()
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
